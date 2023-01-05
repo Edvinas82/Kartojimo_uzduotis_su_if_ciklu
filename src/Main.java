9be8844d69c09d66664b8892486d7e95ee7a092f
Edvinas82 <ziukoslenis@gmail.com>
2023-01-05 09:45:06 +0200

    Padariau 2 kartojimo užduotį su if ciklu

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-
         boolean x = arNeigaiamasArTeigiamas(40);
-
         int y = -25;
-
         boolean z = arNeigaiamasArTeigiamas(y);
-
         System.out.println(z);
-
         System.out.println(x);
-
         System.out.println(arNeigaiamasArTeigiamas(125));
-
-
-        arNeigaiamasArTeigiamas(40);
-        arNeigaiamasArTeigiamas(-25);
-        arNeigaiamasArTeigiamas(125);
-
+        //2uzd
+        int a = 60;
+        int b = 31;
+        arLyginisArNelyginis(a);
+        arLyginisArNelyginis(b);
     }
-        public static boolean arNeigaiamasArTeigiamas(int x) {
-
-            if (x < 0) return true;
-            else {
-                return false;
-            }
+    public static boolean arNeigaiamasArTeigiamas(int x) {
+        if (x < 0) return true;
+        else {
+            return false;
+        }
+    }
+    public static void arLyginisArNelyginis(int x) {
+        if (x % 2 == 0){
+            System.out.println("Lyginis");
+        }
+        else {
+            System.out.println("Nelyginis");
+        }
     }
 }
